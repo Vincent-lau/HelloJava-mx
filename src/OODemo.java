f1a5c80a5e1042097c2ef2b15b0883049a98c7c2
Shuntian Liu <shuntian.liu@outlook.com>
2023-07-29 23:08:21 +0100

    list

--- src/OODemo.java
+++ src/OODemo.java
@@ -1,5 +1,6 @@
 import jdk.jshell.spi.ExecutionControl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OODemo {
@@ -27,21 +28,40 @@ public class OODemo {
     }
 
     // "abcd"    -----> ['a', 'b', 'c', 'd']
-    public static List<Character> toArray(String s) {
+    // [0, 3) ---> [0, 1, 2]
+    public static List<String> toArray(String s) {
+        List<String> aabbcc = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++ ) {
+            String oo = s.substring(i, i+1);
+            aabbcc.add(oo);
+        }
+        return aabbcc;
+
+    }
+    
+    // n = 3, x = "ab" ----> ["ab", "ab", "ab"]
+    public static List<String> filln(int n, String x) {
+       return null; 
+    }
+
+    // [1, 3, 5, 6] ---> [6, 1, 3, 5]
+    public static List<Integer> shuffle(List<Integer> l) {
+       return null;
 
 
     }
 
     public static void main(String[] args) {
-        String s = "abceeeeeeeeeeed";
-        String t = fl(s);
-        int x = 10;
-        int y = 11;
-        boolean z = calculate(10, 11);
-        System.out.println(t);
-
-
+//        String s = "abceeeeeeeeeeed";
+//        String t = fl(s);
+//        int x = 10;
+//        int y = 11;
+//        boolean z = calculate(10, 11);
+//        System.out.println(t);
+        System.out.println(toArray("abcde"));
     }
 
 
 }
+
